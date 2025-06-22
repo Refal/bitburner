5dc86751c01a628df79769b469ba9618bd2946e5
Egor <4848778+Refal@users.noreply.github.com>
2025-06-22 23:06:38 +0200

    Create worker-hack.js

---
+++ worker-hack.js
@@ -0,0 +1,12 @@
+/** @param {NS} ns **/
+export async function main(ns) {
+  const target = ns.args[0];
+  const threads = ns.args[1] || ns.getRunningScript().threads;
+  const offset = ns.args[2] || 0;
+
+  if (offset > 0) {
+    await ns.sleep(offset);
+  }
+  
+  await ns.hack(target, { threads: Number(threads) || 1 });
+}
